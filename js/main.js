9c9ccc98ffe1bef804d68b854ca255431b2843ca
sollan0506 <sollan0506@naver.com>
2021-01-06 12:14:50 +0900

    gnb pc버전 구현 완료
    반응형은 아직 미적용

--- js/main.js
+++ js/main.js
@@ -6,4 +6,33 @@ $(function(){
         arrows: false,
         dotsClass: 'slick-dots'
     });
+
+    // gnb 주메뉴 붉은선, gnb_hidden 보이기/숨기기
+    $(".gnb > ul > li, .gnb_bg").hover(
+        function () {
+            $(this).addClass("active");
+            $(".gnb_hidden").css("display", "block");
+            $(".gnb_bg").css("display", "block");
+            
+            // display : none 에서 display : block 으로 될텐데 
+            // 이때 slick 슬라이더의 너비와 높이가 지정되지 않는 오류가 생긴다.
+            // slick의 위치를 수동으로 새로 고쳐줘야 한다.
+            $('.gnb_slick').slick('setPosition'); 
+        }, 
+        function () {
+            $(this).removeClass("active");
+            $(".gnb_hidden").css("display", "none");
+            $(".gnb_bg").css("display", "none");
+        }
+    );
+
+    // gnb 서브메뉴 붉은선
+    $(".gnb .gnb_hidden > ul > li > a").hover(
+        function () {
+            $(this).addClass("active");
+        }, 
+        function () {
+            $(this).removeClass("active");
+        }
+    );
 });
